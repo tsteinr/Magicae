2f49248c3b6a2ab9def77771e3790942557813fa
Thomas <Thomas@THOMAS-PC>
2017-02-24 05:49:27 +0100

    N/A

---
+++ Magicae/Data/Spell.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Magicae.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Table("Spells")]
+    public class Spell
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key, Column("ID")]
+        public int Id { get; set; }
+    }
+}
